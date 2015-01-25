4fc9d110130b6f4fe5539c5f0edde8e0018e470d
Oscar <od_cortez@galileo.edu>
2015-01-24 18:40:01 -0600

    Add seach and filter with AngularJS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // Javascript Code.
+
+var angularProject = angular.module('AngularProject', []);
+
+angularProject.filter("pictureSong", function(){
+	return function(item){
+		return (JSON.stringify(item)).slice(10,-18);
+	};
+});
+
+angularProject.controller('SearchController', function ($scope, $http) {
+  $http.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=85b8c37b1a6be5182a5ed0549c4a7400&format=json').success(function(data) {
+    $scope.phones = data.tracks.track;   
+
+ });
+
+$scope.orderBy = function(orden){
+	$scope.orderSelect = orden;
+};
+  
+});
+ 
---
+++ js/tests.js
@@ -0,0 +1,5 @@
+describe("AngularJS testing", function(){
+	it ("should load json data", function(){
+		expect(dataTable.information).not.toBeGreaterThan(null);
+	});
+})
